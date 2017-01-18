8731bca55fe0d091753de671b49b36eeaa49b10a
Zach Kolodny <zach.kolodny@gmail.com>
2017-01-18 20:01:20 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,13 @@
 # Write your #display_rainbow method here
+def display_rainbow(colors)
+  x = 0
+  colors.size.times do
+    print "#{colors[x][0].upcase}: #{colors[x]}"
+    x = x + 1
+    if x == colors.size
+      puts
+    else
+      print ", "
+    end
+  end
+end
