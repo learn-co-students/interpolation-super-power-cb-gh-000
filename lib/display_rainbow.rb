75c350afd25e07905dc27e5fa89776770315df5f
Kabelo Moiloa <moiloakabelo@gmail.com>
2017-01-25 13:25:45 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,8 @@
 # Write your #display_rainbow method here
+def display_rainbow(colors)
+  rainbow = ""
+  for color in rainbow:
+    rainbow = rainbow + "#{color[0].upcase}: #{color}"
+  end
+  rainbow
+end
