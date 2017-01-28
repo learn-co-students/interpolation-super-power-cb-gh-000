b5ed26cbec20615cdcb88c50b2cf868899f17d15
Dídac González <didac.gonza@gmail.com>
2017-01-28 16:14:06 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,4 @@
 # Write your #display_rainbow method here
+def display_rainbow(carray)
+  puts "R: #{carray[0]}, O: #{carray[1]}, Y: #{carray[2]}, G: #{carray[3]}, B: #{carray[4]}, I: #{carray[5]}, V: #{carray[6]}"
+end
