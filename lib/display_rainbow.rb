a11b3155290e7ea82b2ff8ca30b862d6025615cc
Nithin Sade <sadenithin@gmail.com>
2017-02-17 04:51:06 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,8 @@
 # Write your #display_rainbow method here
+
+def display_rainbow(colors)
+  index = ["R","O","Y","G","B","I","V"]
+
+    print "#{index[0]}: #{colors[0]}, #{index[1]}: #{colors[1]}, #{index[2]}: #{colors[2]}, "
+    print "#{index[3]}: #{colors[3]}, #{index[4]}: #{colors[4]}, #{index[5]}: #{colors[5]}, #{index[6]}: #{colors[6]}\n"
+end
