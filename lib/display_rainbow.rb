112e5565051a942fe25bf503bfd01fedf36f0ad8
Dyl Bromell <dyl@resonategroup.ca>
2017-05-05 22:32:18 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,13 @@
 # Write your #display_rainbow method here
+def display_rainbow(colors)
+  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+end
+# def display_rainbow(colors)
+#   colors.each_with_index do |i, index|
+#     if index < colors.size - 1
+#       print "#{i.upcase.chars.first}: #{i}, "
+#     elsif index == colors.size - 1
+#       print "#{i.upcase.chars.first}: #{i} "
+#     end
+#   end
+# end
