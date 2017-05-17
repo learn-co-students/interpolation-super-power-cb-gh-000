f392c8fdd9399f9db53eb845a36d639bbea3626a
Sam Ebertz <samuel.ebertz@gmail.com>
2017-05-17 20:00:10 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,6 @@
 # Write your #display_rainbow method here
+def display_rainbow(colors)
+  rainbow = ""
+  colors.size.times {|i| rainbow += "#{colors[i][0].upcase}: #{colors[i]}, "}
+  puts rainbow[0,rainbow.size-2]
+end
