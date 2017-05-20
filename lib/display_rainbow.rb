e1f3858d003407c97d71c80204d30476cedc28b3
Wyndell Bibera <wmbibera@up.edu.ph>
2017-05-20 03:11:13 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,4 @@
 # Write your #display_rainbow method here
+def display_rainbow(colors)
+  puts "#{colors[0][0, 1].upcase}: #{colors[0]}, #{colors[1][0, 1].upcase}: #{colors[1]}, #{colors[2][0, 1].upcase}: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+end
