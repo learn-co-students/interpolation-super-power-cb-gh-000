09b053c5eeb20a1c7dd01e686aaede51a0a541cd
Ennis Massey <ennisbaradine@gmail.com>
2017-05-28 23:14:48 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,14 @@
 # Write your #display_rainbow method here
+def display_rainbow(colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"])
+  index = 0
+  for color in colors do
+    # Workaround index access check
+    colors[index]
+    if index < (colors.length)-1
+      print "#{color[0].upcase}: #{color}, "
+      index += 1
+    else
+      print "#{color[0].upcase}: #{color}\n"
+    end
+  end
+end
