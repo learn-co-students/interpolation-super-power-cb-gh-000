687541564fb2633ad5f2f1a3308e6b13b184b818
Esphas Kueen <esphas@hotmail.com>
2017-06-21 14:22:40 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,8 @@
 # Write your #display_rainbow method here
+def display_rainbow colors
+  colors.each_index do |i| colors[i] end
+  output = colors.map do |color|
+    color[0].upcase + ': ' + color
+  end.join(', ') + "\n"
+  puts output
+end
