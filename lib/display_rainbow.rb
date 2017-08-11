69b67b40641a6d76fea3c652f0bb2a14ebd11ca4
Saujan Ghimire <supertrampsookah@gmail.com>
2017-08-11 08:45:09 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -6,14 +6,14 @@ end
 
 # Write your #display_rainbow method here
 def display_rainbow(colors)
-  output = ""
-  colors.each_with_index { |e, index|
-    output += e.initial.upcase + ': ' + e
-    if index < colors.size - 1
-      output += ", "
-    end
-    }
-  puts output
+  # output = ""
+  # colors.each_with_index { |e, index|
+  #   output += e.initial.upcase + ': ' + e
+  #   if index < colors.size - 1
+  #     output += ", "
+  #   end
+  #   }
+  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
 end
-
+# "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet\n"
 # display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
