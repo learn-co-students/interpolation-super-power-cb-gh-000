5d1546328b9d1e1513935512fcccc04ca7e45f0c
Andrew Malsbury <andymalsb@gmail.com>
2017-08-20 02:17:09 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,15 @@
 # Write your #display_rainbow method here
+def display_rainbow(colors)
+
+  str = ""
+  colors_and_first_letter = []
+
+  colors.each{|i|
+    first_letter = i[0,1].upcase
+    colors_and_first_letter.insert(-1, "#{first_letter}: #{i}")
+  }
+
+  str = colors_and_first_letter.join(", ")
+  puts"#{str}"
+
+end
