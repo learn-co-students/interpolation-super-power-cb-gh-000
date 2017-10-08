ab94a871a4dacff314a9770613b2c6bf21e8b1dc
Tiago Severino <tiago.mseverino@gmx.com>
2017-10-08 22:20:06 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,26 @@
 # Write your #display_rainbow method here
+def display_rainbow(colors)
+  color = colors[0]
+  a(color)
+  color = colors[1]
+  a(color)
+  color = colors[2]
+  a(color)
+  color = colors[3]
+  a(color)
+  color = colors[4]
+  a(color)
+  color = colors[5]
+  a(color)
+  color = colors[6]
+  a(color, true)
+end
+
+def a(ab, last = false)
+  print "#{ab[0].upcase}: #{ab}"
+  if !last
+    print ", "
+  else
+    print "\n"
+  end
+end
