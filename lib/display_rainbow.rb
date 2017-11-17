416f0dfd57046605191e873a653946d4fa8bc16a
Wilmer Perez <wilmer.perez18@myhunter.cuny.edu>
2017-11-17 21:58:25 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,12 @@
 # Write your #display_rainbow method here
+def display_rainbow(colors)
+  index = 0
+  begin
+    if index < colors.length - 1
+      print colors[index][0].upcase + ": " + colors[index] + ", "
+    else
+      puts colors[index][0].upcase + ": " + colors[index]
+    end
+    index = index + 1
+  end while index < colors.length
+end
