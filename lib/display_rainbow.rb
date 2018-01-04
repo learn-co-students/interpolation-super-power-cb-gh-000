0972ba4e1d7043b731d91193dfcd2f9c0ba1997a
Jonathan Bouchard <jonathaneutron@hotmail.com>
2018-01-04 04:31:21 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,12 @@
 # Write your #display_rainbow method here
+def display_rainbow(colors)
+  colors.each_with_index do |x, index|
+    if index != colors.size - 1
+        print "#{x.chr.upcase}: #{x}, "
+    else
+        print "#{x.chr.upcase}: #{x}\n"
+    end
+  end
+end
+
+display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
