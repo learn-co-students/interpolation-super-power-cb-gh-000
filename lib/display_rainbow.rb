301d52b59a7b31b979ed8d8c462464d53bda3552
Sreekar Chigurupati <chigurupatisreekar@gmail.com>
2018-02-14 10:52:21 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,13 @@
-def display_rainbow(colors=['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
-  mystring= ""
-  colors.each do |c|
-    mystring << "#{c[0].upcase()}: #{c}, "
-  end
-  puts mystring[0..-3]
+def display_rainbow(colors)
+  letters = ['R', 'O', 'Y', 'G', 'B', 'I', 'V']
+  print "#{letters[0]}: #{colors[0]}, "
+  print "#{letters[1]}: #{colors[1]}, "
+  print "#{letters[2]}: #{colors[2]}, "
+  print "#{letters[3]}: #{colors[3]}, "
+  print "#{letters[4]}: #{colors[4]}, "
+  print "#{letters[5]}: #{colors[5]}, "
+  puts "#{letters[6]}: #{colors[6]}"
 end
+
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+display_rainbow(colors)
