4c9a7b2ebc70a0e169235cc21dffd93b2a7b45c4
Sreekar Chigurupati <chigurupatisreekar@gmail.com>
2018-02-14 10:50:41 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,7 @@
-# Write your #display_rainbow method here
+def display_rainbow(colors=['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+  mystring= ""
+  colors.each do |c|
+    mystring << "#{c[0].upcase()}: #{c}, "
+  end
+  puts mystring[0..-3]
+end
