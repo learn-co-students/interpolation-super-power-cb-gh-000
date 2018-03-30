fcf79783645bb04e24479a25c9258a7a9dbf190f
Damitha Ranasingha <rmddcr@gmail.com>
2018-03-30 10:42:43 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,4 @@
 # Write your #display_rainbow method here
 def display_rainbow(input_array)
-
-puts  "R: #{input_array[0]}, O: #{input_array[1]}, Y: #{input_array[2]}, G: #{input_array[3]}, B: #{input_array[4]}, I: #{input_array[5]}, V: #{input_array[6]}"
+  puts  "R: #{input_array[0]}, O: #{input_array[1]}, Y: #{input_array[2]}, G: #{input_array[3]}, B: #{input_array[4]}, I: #{input_array[5]}, V: #{input_array[6]}"
 end
