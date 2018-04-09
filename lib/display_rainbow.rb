7f1e49790f8ef04c4f5d78bacabb8ef00374db2d
Xavier Codinas Bañeres <xavier19966@gmail.com>
2018-04-09 10:33:40 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,12 @@
 # Write your #display_rainbow method here
+def display_rainbow(colors)
+  string = ""
+  string += "R: #{colors[0]}, "
+  string += "O: #{colors[1]}, "
+  string += "Y: #{colors[2]}, "
+  string += "G: #{colors[3]}, "
+  string += "B: #{colors[4]}, "
+  string += "I: #{colors[5]}, "
+  string += "V: #{colors[6]}"
+  puts string
+end
