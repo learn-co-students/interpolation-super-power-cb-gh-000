97c3e70c5ba29e630ff9fe8af4e67e2a1a396e90
Griffin Pfenniger <pfenniger.griffin@gmail.com>
2018-09-20 19:24:42 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,13 @@
 # Write your #display_rainbow method here
+def display_rainbow (colours)
+  index = 0
+  str = ""
+  colours.length.times {
+    str += "#{colours[index][0].upcase}: #{colours[index]}"
+    index += 1
+    if index != colours.length
+      str += ", "
+    end
+  }
+  puts str
+end
