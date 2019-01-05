efa2a1ef86d7c75004b23e79dc3c5ce08e90b3ed
Mehmet Yıldırım <xmehmetyz@gmail.com>
2019-01-05 18:09:19 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 # Write your #display_rainbow method here
-"R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
+
 def display_rainbow(arr)
 puts "R: #{arr[0]}, O: #{arr[1]}, Y: #{arr[2]}, G: #{arr[3]}, B: #{arr[4]}, I: #{arr[5]}, V: #{arr[6]} "
 end
