b3e6dc6d9b3d355c34368970f130e76602763aef
Damion Mounts <damionotg@gmail.com>
2019-05-15 00:16:37 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,18 @@
 # Write your #display_rainbow method here
+def display_rainbow(colors)
+
+  # Loop through all colors
+  for i in 0..colors.length-1 do
+
+    # No comma and do  newline for last color
+    if i == colors.length-1
+      puts colors[i][0].upcase + ": " + colors[i]
+
+    # For all colors, print {first letter caps: name,}
+    else
+      print colors[i][0].upcase + ": " + colors[i] + ", "
+    end
+
+  end
+
+end
