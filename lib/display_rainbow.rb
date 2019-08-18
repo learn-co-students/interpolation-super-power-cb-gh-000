ddacf15229dfde20d9f23af6d3fb34892c83a9fa
Mohamed Semega <mouhamedbocar@yahoo.fr>
2019-08-18 12:44:25 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,4 @@
 # Write your #display_rainbow method here
+def display_rainbow(list=[])
+  puuts "#{list[0][0].uppercase: list[0]}, #{list[1][0].uppercase: list[1], list[2][0].uppercase:list[2], list[3][0].uppercase:list[3],list[4][0].uppercase:list[4], list[5][0].uppercase:list[5],list[6][0].uppercase:list[4]}"
+end
