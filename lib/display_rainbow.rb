9411503411701c7bf3862a06a1c54c8155ad137f
Faranak Maddah <faranak1015@gmail.com>
2019-08-22 12:26:56 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,6 @@
 # Write your #display_rainbow method here
 def display_rainbow
-  ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+  ['R:red', 'O: orange', 'Y: yellow', 'G: green', 'B: blue', 'I: indigo', 'V: violet']
 
 print(display_rainbow)
 end
