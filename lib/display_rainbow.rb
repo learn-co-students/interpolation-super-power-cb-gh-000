ab745a1db84ad393bfec30c90ff39aed0003e200
Faranak Maddah <faranak1015@gmail.com>
2019-08-28 16:25:08 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -13,6 +13,7 @@ puts "The seven letter in the alphabet is: #{colors[6]}"
 
 def display_rainbow(colors)
 
-print ("display_rainbow")
+#print ("display_rainbow")
+puts "R: #{colors[0]}, O: #{colors[1]} Y: #{colors[2]} G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
 
 end
