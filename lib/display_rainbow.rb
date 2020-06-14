c63c6f5a084bfb6622792f930690af0fca627475
Omar Villa <o.villanueva@thearcane.com>
2020-06-14 03:10:30 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,10 @@
 # Write your #display_rainbow method here
+def display_rainbow(colors)
+  strArr = []
+
+  colors.each do |color|
+    strArr << "#{color[0,1].upcase}: #{color}"
+  end
+
+  puts strArr.join(', ')
+end
